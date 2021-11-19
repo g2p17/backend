f948274f85c89ae94325a8931949e22c5def7361
daorjuelah <daorjuelah@unal.edu.co>
2021-11-19 00:49:30 -0500

    corrección leve settings

--- user_auth_ms/settings_dev.py
+++ user_auth_ms/settings_dev.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'user_auth_ms.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -122,13 +122,14 @@ DATABASES = {
 }
 
 """
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
