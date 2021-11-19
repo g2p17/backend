201ad9113ddcf9c42951dfc86d011ccc72875a0c
juan13ut <juan13u@gmail.com>
2021-11-19 15:58:44 +0000

    GP-5 Add end points corresponding to get/update/delete user by id

--- user_auth_ms/urls.py
+++ user_auth_ms/urls.py
@@ -6,7 +6,10 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user-customer/', views.UserCreateView.as_view()),
-    path('user-admin/', views.UserCreateView.as_view()),     
+    path('user-admin/', views.UserCreateView.as_view()),
+    path('user-customer/detail/<int:pk>/', views.UserDetailView.as_view()),
+    path('user-customer/update/<int:pk>/', views.UserUpdateView.as_view()),
+    path('user-customer/delete/<int:pk>/', views.UserDeleteView.as_view()),      
     path('verifyToken/',   views.VerifyTokenView.as_view()),
     path('refresh/',   TokenRefreshView.as_view()),
     path('login/',         TokenObtainPairView.as_view()),
