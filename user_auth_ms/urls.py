b07d685ff102f13796e479d290cbe0fb649cb878
juan13ut <juan13u@gmail.com>
2021-11-19 03:27:12 +0000

    GP-20 web service to login user using simplejwt views

--- user_auth_ms/urls.py
+++ user_auth_ms/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from users  import views
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user-customer/', views.UserCreateView.as_view()),
     path('verifyToken/',   views.VerifyTokenView.as_view()),
     path('refresh/',   TokenRefreshView.as_view()),
+    path('login/',         TokenObtainPairView.as_view()),
 ]
