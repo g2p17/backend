581928292d0c8d3b406802dd3c85558b87e82c4e
juan13ut <juan13u@gmail.com>
2021-11-19 05:03:06 +0000

    GP-28 Add an independent endpoint to register admin user

--- user_auth_ms/urls.py
+++ user_auth_ms/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user-customer/', views.UserCreateView.as_view()),
+    path('user-admin/', views.UserCreateView.as_view()),     
     path('verifyToken/',   views.VerifyTokenView.as_view()),
     path('refresh/',   TokenRefreshView.as_view()),
     path('login/',         TokenObtainPairView.as_view()),
