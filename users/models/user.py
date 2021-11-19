b6f8af2e79ccab5bf126614dd2cacfe96b03d7f2
juan13ut <juan13u@gmail.com>
2021-11-19 17:11:39 +0000

    GP-28 Update model user to manage user roles

--- users/models/user.py
+++ users/models/user.py
@@ -30,6 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     password = models.CharField('Password', max_length=256)
     name     = models.CharField('Name',     max_length=50)
     email    = models.EmailField('Email',   max_length=100, unique=True)
+    role    = models.CharField('Role',   max_length=15, default="customer")
     
     def save(self, **kwargs):
         some_salt = 'mMUj0DrIK6vgtdIYepkIxN'
--- users/serializers/userSerializer.py
+++ users/serializers/userSerializer.py
@@ -5,13 +5,14 @@ from rest_framework       import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model  = User
-        fields = ['id', 'username', 'password', 'identity_document',  'phone_number', 'name', 'email']
+        fields = ['id', 'username', 'password', 'identity_document',  'phone_number', 'name', 'email', 'role']
 
     def to_representation(self, obj):
         user    = User.objects.get(id=obj.id)
         return {
             'id'      : user.id,
             'name'    : user.name,
+            'role'    : user.role,
             'email'   : user.email,
             'username': user.username,
             'phone_number': user.phone_number,
